dbfd6df5d935bf17b5d7a2af3c3c8fd5f8e0e259
AndreyAboba <86691034+AndreyAboba@users.noreply.github.com>
2025-12-16 07:35:38 +0300

    Update xyinya.lua

--- xyinya.lua
+++ xyinya.lua
@@ -935,7 +935,7 @@ function Visuals.Init(UI, Core, notify)
         end
         
         -- Информация (страна/устройство) идет между именем и барами/текстом
-        positions.Info = positions.Name - 5 * textScale
+        positions.Info = positions.Name + 5 * textScale
         
         -- Проверка границ экрана
         for name, y in pairs(positions) do
